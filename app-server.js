600b0b74b71fe7b90fa9b1e3a9c19c5d0bc8007c
Juan Zelaya Castillo <zjuan4101@gmail.com>
2024-03-10 23:25:50 -0400

    setup app-server to user the routes for users and posts

--- app-server.js
+++ app-server.js
@@ -13,8 +13,8 @@ app.use((req, res, next) => {
 app.use(logger('dev'))
 app.use(favicon(path.join(__dirname, 'public', 'img','logo.png')))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/api/todos', require('./routes/api/todos'))
-// http://localhost:8000/api/todos/completed
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/posts', require('./routes/api/posts'))
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
