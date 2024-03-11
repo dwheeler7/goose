e68b657e196060b06c00debd196093ef6bce90d1
Juan Zelaya Castillo <zjuan4101@gmail.com>
2024-03-10 21:00:42 -0400

    Created base model for posts

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    name: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    githublink: {type: String, required: true },
+    content: { type: String, required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    createdAt: { type: Date, default: Date.now }
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
