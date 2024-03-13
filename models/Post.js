290134163929607c23870fdf97a39a714b11ce1a
Danny Wheeler <wheeler@Dannys-MacBook-Pro.local>
2024-03-12 18:33:23 -0700

    Juan updates to Post schema

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const postSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-    githublink: {type: String, required: true },
-    content: { type: String, required: true },
+    githublink: {type: String},
+    description: { type: String, required: true },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 }, {
     timestamps: true
