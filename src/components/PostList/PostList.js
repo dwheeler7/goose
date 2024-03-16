32dbfdaa06eedc3acc3563859a5d8969bab0e565
Isaiah Branstetter <isaiah.bran@icloud.com>
2024-03-16 15:38:50 -0400

    Updated PostList.js

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,24 +1,69 @@
 import styles from './PostList.module.scss'
 import Post from '../Post/Post'
- 
-// export default function PostList({ posts }) {
-//     const items = posts.map(item =>
-//       <Post
-//         key={item._id}
-//         post={item}
-//       />
-//     )
-//     return (
-//       <main className={styles.PostList}>
-//         {items}
-//       </main>
-//     )
-//   }
+import React, { useState } from 'react'
 
-export default function PostList(){ 
-    return(
-        <div className={styles.PostList}>
-            <Post />
-        </div>
-    )
+export default function PostList(props) {
+    const [postData, setPostData] = useState({
+        title: '',
+        description: '',
+        gitHubLink: '',
+        image: null,
+    });
+    const handleChange = (e) => {
+        const { name, value, files } = e.target;
+        if (name === 'image') {
+            setPostData({ ...postData, [name]: files[0] });
+        } else {
+            setPostData({ ...postData, [name]: value });
+        }
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await props.createPost(postData, props.token);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return (
+        <form className={styles.post} onSubmit={handleSubmit}>
+            <h2 className={styles.heading}>Upload A New Post {props.user.name}</h2>
+            <input
+                type="text"
+                name="title"
+                value={postData.title}
+                onChange={handleChange}
+                className={styles.input}
+                placeholder='Title'
+                required
+            />
+            <input
+                type="text"
+                name="description"
+                value={postData.description}
+                onChange={handleChange}
+                className={styles.input}
+                placeholder='Description'
+                required
+            />
+            <input
+                type="text"
+                name="gitHubLink"
+                value={postData.gitHubLink}
+                onChange={handleChange}
+                className={styles.input}
+                placeholder='GitHub Link'
+                required
+            />
+            <input
+                type="file"
+                name="image"
+                onChange={handleChange}
+                className={styles.input}
+            />
+            <button type="submit" className={styles.submitBtn}>
+                Post
+            </button>
+        </form>
+    );
 }
