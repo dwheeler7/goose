ad6eddb334ad7f59f425349d02cfa062a8d850d1
Jcasanova1990 <jeremykcasanova@gmail.com>
2024-03-16 18:14:46 -0400

    Added file Loader to Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,75 @@
 const path = require('path');
-const webpack = require('webpack');
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 module.exports = env => {
-    return {
-        entry: {
-            App: './src/index.js',
-            main: './src/main.js'
-        },
-        output: {
-            path: path.resolve(__dirname, 'public/js/dist'),
-            publicPath: '/js/dist', // This line is needed for multiple paths
-            filename: '[name].[contenthash].js',
-            sourceMapFilename: "[name].[contenthash].js.map"
-        },
-        devtool: "source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.m?js$/,
-                    enforce: 'pre',
-                    use: ['source-map-loader'],
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: ['babel-loader']
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        'style-loader',
-                        {
-                            loader: "css-loader",
-                            options: {
-                                modules: true,
-                            },
-                        },
-                        'sass-loader',
-                        'postcss-loader'
-                    ]
-                },
-                {
-                    test: /\.(png|jpe?g|gif)$/i,
-                    use: [
-                        {
-                            loader: 'file-loader', //added file loader
-                            options: {
-                                name: '[name].[contenthash].[ext]',
-                                outputPath: 'images',
-                            },
-                        },
-                    ],
-                },
-            ]
-        },
-        resolve: {
-            modules: [
-                'node_modules'
-            ]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                inject: true,
-                hash: true,
-                template: './src/index.html',
-                children: false,
-                filename: '../../index.html'
-            }),
-            new CleanWebpackPlugin(),
-            new webpack.ProvidePlugin({
-                'React': 'react'
-            })
-        ],
-        optimization: {
-            splitChunks: {
-                chunks: 'all',
-                minSize: 0
-            },
-            usedExports: true,
-            sideEffects: true
-        }
-    };
+	return {
+		entry: {
+			App: './src/index.js',
+			main: './src/main.js'
+		},
+		output: {
+			path: path.resolve(__dirname, 'public/js/dist'),
+			publicPath: '/js/dist', // This line is needed for multiple paths
+			filename: '[name].[contenthash].js', // '[name].[contenthash].js' put this if you want to get hashed files to cache bust
+			sourceMapFilename: "[name].[contenthash].js.map"
+		},
+		devtool:"source-map",
+		module: {
+			rules: [
+				{
+					test: /\.m?js$/,
+					enforce: 'pre',
+					use: ['source-map-loader'],
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: ['babel-loader']
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						'style-loader',
+						{
+							loader: "css-loader",
+							options: {
+								modules: true,
+							},
+						},
+						'sass-loader',
+						'postcss-loader'
+					]
+				}
+			]
+		},
+		resolve: {
+			modules: [
+				'node_modules'
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				inject: true,
+				hash: true,
+				template: './src/index.html',
+				children: false,
+				filename: '../../index.html'
+			}),
+			new CleanWebpackPlugin(),
+			new webpack.ProvidePlugin({
+				'React': 'react'
+			})
+		],
+		optimization: {
+			splitChunks: {
+				chunks: 'all',
+				minSize: 0
+			},
+			usedExports: true,
+			sideEffects: true
+		}
+	};
 };
