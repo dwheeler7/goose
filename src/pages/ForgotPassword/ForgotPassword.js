1d365b954185052e4cb0b683f7e631dc777fd31f
Danny Wheeler <wheeler@Dannys-MacBook-Pro.local>
2024-03-24 13:25:56 -0700

    removed unused code

--- src/pages/ForgotPassword/ForgotPassword.js
+++ src/pages/ForgotPassword/ForgotPassword.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import ForgotPasswordForm from '../../components/ForgotPasswordForm/ForgotPasswordForm'; // Adjust the import path as needed
 import styles from './ForgotPassword.module.scss'; // Adjust the import path as needed
 
-export default function ForgotPasswordPage({ setUser }) {
-    const navigate = useNavigate();
+export default function ForgotPasswordPage({ setUser }) {    
   
     return (
       <div className={styles.container}>
