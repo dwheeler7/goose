c510835adadae9fabcc71f7a4b5340d86789ae44
Danny Wheeler <wheeler@Dannys-MacBook-Pro.local>
2024-03-24 14:07:32 -0700

    cleanup unused code

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -4,8 +4,7 @@ import styles from './AuthPage.module.scss';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 
-export default function AuthPage({ setUser, user }) {
-  const navigate = useNavigate()
+export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);    
 
   return (
